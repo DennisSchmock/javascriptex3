74c03dbe48d5c8d1f76ded60693216acb81bfeaf
daeniz <dennis@schmock.eu>
2016-09-22 13:26:04 +0200

    Init

--- src/main/webapp/js/task4.js
+++ src/main/webapp/js/task4.js
@@ -4,21 +4,42 @@
  * and open the template in the editor.
  */
 $(document).ready(function () {
-   var imgArray = $("img")
+    var imgArray = $("img");
+
+    shuffle(imgArray);
+
+    // window.console.log(imgArray[1]);
+//    for (var i = 0; i < imgArray.length; i++) {
+//        window.console.log(i);
+//    }
+    $('body').on('click', 'img', function () {
         shuffle(imgArray);
-        window.console.log(index);
- 
-    
-    function shuffle(array) {
-    var j, x, i;
-    for (i = a.length; i; i--) {
-        j = Math.floor(Math.random() * i);
-        x = a[i - 1];
-        a[i - 1] = a[j];
-        a[j] = x;
+
+        $("#clear").html("");
+        for (var i = 0; i < imgArray.length; i++) {
+            window.console.log(i);
+            var imgsrc = $(imgArray[i]).attr('src');
+            $("#clear").append("<div class=\"col-sm-4\"><img class=\"img-circle\" style=\"width: 150px;height: 150\" src='" + imgsrc + "'></div>");
+        }
+//      var imgsrc = $(this).attr('src');
+//      var tempImg = $(imgArray[0]);
+//        $(imgArray[0]).replaceWith(this);
+//               $(this).append(imgsrc);
+//                $("html").append("<div id='image_popup'><img src='"+imgsrc+"'></div>");
+
+    });
+    function shuffle(a) {
+        var j, x, i;
+        for (i = a.length; i; i--) {
+            j = Math.floor(Math.random() * i);
+            x = a[i - 1];
+            a[i - 1] = a[j];
+            a[j] = x;
+        }
     }
-}
-    
+    ;
+
+
 });
 
 
